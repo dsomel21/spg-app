6ac576e81f57286a76fc9c07a4eaa0c6b4f9ac41
Dilraj <dsomel21@gmail.com>
2019-06-05 21:39:49 -0400

    Set up ENV variables. Working on getting my bucket data

---
+++ app.js
@@ -0,0 +1,20 @@
+const ENV = require('dotenv').config().parsed
+const aws = require('aws-sdk')
+
+// const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID
+
+// This enables promises in AWS SDK
+aws.config.setPromisesDependency()
+aws.config.update({
+  accessKeyId: ENV.AWS_ACCESS_KEY_ID,
+  secretAccessKey: ENV.AWS_SECRET_ACCESS_KEY,
+  region: 'us-east-1'
+})
+
+const s3 = new aws.S3()
+const response = s3.listObjectsV2({
+  Bucket: 'spg-audio'
+})
+
+console.log(response.response)
+debugger
