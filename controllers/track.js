9586b0046d221cb98334a4b1cd61f4c2ff79f586
Dilraj <dsomel21@gmail.com>
2019-07-04 17:49:30 -0400

    Finally created a record in the database

--- controllers/track.js
+++ controllers/track.js
@@ -3,7 +3,17 @@ const Track = require('../models/track')
 exports.createTrack = (req, res) => {
   console.log('HELLLOOO WORLLLD!')
   console.log(req.body)
-  debugger
+  const newTrack = new Track({
+    name: req.body.name,
+    description: req.body.description
+  })
+  newTrack.save((data, err) => {
+    if (err) {
+      console.log(`Error saving. ${err}`)
+      return res.status(400)
+    }
+    res.json(data)
+  })
   // const
 }
 
